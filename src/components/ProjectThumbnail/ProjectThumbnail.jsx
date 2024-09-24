a8bec25c529aee4d53683bd69ecfd017b06b8b5a
jasin1 <jasin.tairaidrissi@gmail.com>
2024-09-24 19:31:40 +0200

    Thumbnail component

--- src/components/ProjectThumbnail/ProjectThumbnail.jsx
+++ src/components/ProjectThumbnail/ProjectThumbnail.jsx
@@ -1,28 +1,35 @@
-import './ProjectThumbnail.css';
-import Button from '../Button/Button.jsx';
+import "./ProjectThumbnail.css";
+import Button from "../Button/Button.jsx";
 
-function ProjectThumbnail({image, title, description, buttons, isFeatured}){
-  return(
-    <div className={`project-thumbnail ${isFeatured ? 'featured': ''}`}>
-      <img src={image} alt={title} className="project-thumbnail__image" />
-      <div className="project-thumbnail__content">
-        <h3 className="project-thumbnail__title">{title}</h3>
-        <p className="project-thumbnail__description">{description}</p>
+function ProjectThumbnail({ image, title, description, buttons, isFeatured }) {
+  return (
+    <div className={`project-thumbnail ${isFeatured ? "featured" : ""}`}>
+      <div className="img-wrapper">
+        <img src={image} alt={title} className="project-thumbnail__image" />
       </div>
-      <div className="project-thumbnail__buttons">
-        {buttons.map((button, index)=>(
-          <Button
-          key={index}
-          onClick={button.onClick}
-          variant={button.variant || 'default'}
-          type={button.type || 'button'}
-          hasArrow={true}
-          ></Button>
-        ))}
+      <div className="project-thumbnail__content">
+        <div className="title-wrapper">
+          <h3 className="project-thumbnail__title">{title}</h3>
+        </div>
+        <div className="project-thumbnail__text">
+          <p className="project-thumbnail__description">{description}</p>
+          <div className="project-thumbnail__buttons">
+            {buttons.map((button, index) => (
+              <Button
+                key={index}
+                onClick={button.onClick}
+                variant={button.variant || "default"}
+                type={button.type || "button"}
+                hasArrow={true}
+              >
+                {button.text}
+              </Button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-
-  )
+  );
 }
 
 export default ProjectThumbnail;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,40 @@
 import "./Home.css";
 import Header from "../../components/Header/Header.jsx";
 import Button from "../../components/Button/Button.jsx";
+import ThumbNail from "../../components/ProjectThumbnail/ProjectThumbnail.jsx";
+import placeHolderImg from "../../assets/placeHolderImg.jpg";
 
 function Home() {
+  const thumbnailData = {
+    image: placeHolderImg,
+    title: "Garden Genius",
+    description:
+      "Mi a pellentesque proin laoreet risus vulputate quam erat in. Eget orci ullamcorper ut nunc a eget lacus cum. Ac vivamus viverra aliquam duis eu lectus dapibus pulvinar vitae. Pellentesque auctor odio quis venenatis feugiat blandit tortor. Integer purus amet elit odio pharetra commodo. Magnis pharetra odio volutpat",
+    buttons: [{ text: "view Details", variant:"white" }, { text: "GitHub" }],
+    isFeatured: true,
+  };
+
   return (
     <main>
       <article>
         <div className="container">
           <Header />
-          <h1>Home</h1>
-          <Button
-          variant="black"
-          type="button"
-          hasArrow={true}
-          >Let&apos;s Talk</Button>
+          <h1>
+            Crafting Code,<br/> 
+            <span className="Header-span">Designing Experiences</span>
+          </h1>
+          <Button variant="black" type="button" hasArrow={true}>
+            Let&apos;s Talk
+          </Button>
+          <div className="section">
+            <ThumbNail
+              image={thumbnailData.image}
+              title={thumbnailData.title}
+              description={thumbnailData.description}
+              buttons={thumbnailData.buttons}
+              isFeatured={thumbnailData.isFeatured}
+            ></ThumbNail>
+          </div>
         </div>
       </article>
     </main>
