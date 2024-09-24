05d37059a7722d8ce2673105d26b09ebc5e80a12
jasin1 <jasin.tairaidrissi@gmail.com>
2024-09-24 11:38:12 +0200

    Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,17 @@
 import './Button.css';
 
-function Button({children, onClick, type, variant}){
+function Button({children, onClick, type, variant, hasArrow}){
   return(
       <button className={`btn btn-${variant}`} onClick={onClick} type={type}>
           {children}
+          {hasArrow && (
+            <svg width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 8L23 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M16 1L23 8L16 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+            
+
+          )}
       </button>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import "./Home.css";
 import Header from "../../components/Header/Header.jsx";
+import Button from "../../components/Button/Button.jsx";
 
 function Home() {
   return (
@@ -8,6 +9,11 @@ function Home() {
         <div className="container">
           <Header />
           <h1>Home</h1>
+          <Button
+          variant="black"
+          type="button"
+          hasArrow={true}
+          >Let's Talk</Button>
         </div>
       </article>
     </main>
